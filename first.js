58e9d2cfe9c40b85001a11a30a2add505a473f84
Bhi2710 <92436824+Bhi2710@users.noreply.github.com>
2022-01-26 17:15:52 +0530

    Add files via upload

---
+++ first.js
@@ -0,0 +1,18 @@
+var n = 16;
+if(n>10) {
+  console.log("Number is greater than 10 ")
+} else if(n == 10){
+  console.log(" equal 10") 
+}
+
+var a =15;
+var b =11;
+var c =8;
+if(a>=b && a>=c){
+  console.log( a + " number is greater ")
+}else if(b>=a && b>=c){
+  console.log( b + " number is greater ")
+}else{
+  console.log( c + " number is greater")
+}
+
